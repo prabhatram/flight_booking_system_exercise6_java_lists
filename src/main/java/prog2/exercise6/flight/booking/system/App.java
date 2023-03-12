964f02dcd01bfd686bff8f00f0c7f6209f358f4f
prabhatram <prabhat.ram@oulu.fi>
2023-03-12 12:01:09 +0200

    Exercise 6 - Java Lists tempalte

---
+++ src/main/java/prog2/exercise6/flight/booking/system/App.java
@@ -0,0 +1,13 @@
+package prog2.exercise6.flight.booking.system;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
